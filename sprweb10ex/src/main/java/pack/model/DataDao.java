dceead77c302199bd89003bdb8be0d23d3e95e30
mhlee <minhyoungi0208@gmail.com>
2024-07-17 17:31:12 +0900

    20240717 revise

--- sprweb10ex/src/main/java/pack/model/DataDao.java
+++ sprweb10ex/src/main/java/pack/model/DataDao.java
@@ -1,41 +1,32 @@
 package pack.model;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DataDao extends JdbcDaoSupport {  // dataSource와 JDBC 템플릿(쿼리를 위해)을 가지고 있다?
-	
+public class DataDao extends JdbcDaoSupport { // dataSource와 JDBC 템플릿(쿼리를 위해)을 가지고 있다?
+
 	// 상속을 받고 있기 때문에 필드 주입 X, 생성자가 만들어지기 전에 JdbcDaoSupport에 datasource를 전달해야 하기 때문.
 	@Autowired
 	public DataDao(DataSource dataSource) {
 		super.setDataSource(dataSource);
 	}
-	
+
 	public List<JikwonDto> selectJikList(String jik) {
 		String sql = "select jikwon_no,jikwon_name,jikwon_gen,jikwon_pay from jikwon where jikwon_jik=?";
-<<<<<<< HEAD
-		//return (List)getJdbcTemplate().query(sql, new ItemRowMapper());
-		
+
+		// return (List)getJdbcTemplate().query(sql, new ItemRowMapper());
+
 		// 바인딩 변수 배열
-		Object[] 파라미터 = {jik};
+		Object[] 파라미터 = { jik };
 		// 바인딩 변수의 타입 배열
-		int[] argTypes = {java.sql.Types.VARCHAR};
-		
-		return (List)getJdbcTemplate().query(sql, 파라미터, argTypes, (ResultSet rs, int rowNum) -> {
-=======
-		
-		Object[] params = {jik};
-		
-		//return (List)getJdbcTemplate().query(sql, new ItemRowMapper());
-		return (List)getJdbcTemplate().query(sql, params, (ResultSet rs,int rowNum) -> {
->>>>>>> c2e7be3d990ff08f0878ac0880bb1f4e47e5b1f3
+		int[] argTypes = { java.sql.Types.VARCHAR };
+
+		return (List) getJdbcTemplate().query(sql, 파라미터, argTypes, (ResultSet rs, int rowNum) -> {
 			JikwonDto dto = new JikwonDto();
 			dto.setJikwon_no(rs.getString("jikwon_no"));
 			dto.setJikwon_name(rs.getString("jikwon_name"));
