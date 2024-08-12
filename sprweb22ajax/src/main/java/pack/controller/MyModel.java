b13ed30d62fb848df82293e25a042bee32231ab5
mhlee <minhyoungi0208@gmail.com>
2024-08-12 17:16:11 +0900

    20240812 test

---
+++ sprweb22ajax/src/main/java/pack/controller/MyModel.java
@@ -0,0 +1,12 @@
+package pack.controller;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+public class MyModel {
+	private String name;
+	private String skills[];
+}
